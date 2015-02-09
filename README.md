933d02c8ac81d7db08eace545db9480cbfbbe303
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-02-09 21:00:26 +0100

    Moved versioning attributes to a shared file.

--- README.md
+++
@@ -1,2 +0,0 @@
-when commandBus.send(new AddTask(userId: "anders", listId: "x", taskText: "get coffee"));
-then eventBus.raise(new TaskAdded(by: "anders", listId: "x", taskId: "y", "taskText": "get coffee"))
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SlashTodo")]
+[assembly: AssemblyCopyright("Copyright © Anders Fjeldstad 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
