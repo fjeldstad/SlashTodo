ebde2c4772b48fbaf9033e9c7b8a9b9fc4b2e2da
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-02-09 21:43:53 +0100

    Added a first version of the TodoItem class.

---
+++ src/SlashTodo.Core/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlashTodo.Core
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string ListId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? TickedAt { get; set; }
+        public string ClaimedBy { get; set; }
+
+        public bool Done { get { return TickedAt.HasValue; } }
+    }
+}
