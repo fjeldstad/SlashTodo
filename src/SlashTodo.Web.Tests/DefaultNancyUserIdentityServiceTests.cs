494ed9abd7ae5361e7472159a7a01bf44acba90b
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-03-10 22:31:04 +0100

    Implemented DefaultNancyUserIdentityService with tests.

---
+++ src/SlashTodo.Web.Tests/DefaultNancyUserIdentityServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Moq;
+using Nancy;
+using NUnit.Framework;
+using SlashTodo.Infrastructure.AzureTables;
+using SlashTodo.Infrastructure.Configuration;
+using SlashTodo.Web.Security;
+
+namespace SlashTodo.Web.Tests
+{
+    [TestFixture]
+    public class DefaultNancyUserIdentityServiceTests
+    {
+        private readonly CloudStorageAccount _storageAccount = CloudStorageAccount.Parse((new AzureSettings(new AppSettings())).StorageConnectionString);
+        private Mock<INancyUserIdentityIdProvider> _idProviderMock;
+        private DefaultNancyUserIdentityService _service;
+        
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _idProviderMock = new Mock<INancyUserIdentityIdProvider>();
+            _service = new DefaultNancyUserIdentityService(
+                _storageAccount,
+                _idProviderMock.Object,
+                string.Format("test{0:N}", Guid.NewGuid()),
+                string.Format("test{0:N}", Guid.NewGuid()));
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            var table = _storageAccount.GetTable(_service.UserIdentityByIdTableName);
+            table.DeleteIfExists(); 
+            table = _storageAccount.GetTable(_service.UserIdentityBySlackUserIdTableName);
+            table.DeleteIfExists();
+        }
+
+        [Test]
+        public async Task GetOrCreateInsertsRowsWhenNotExistsAndReturnsUserIdentity()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _idProviderMock.Setup(x => x.GenerateNewId()).Returns(id);
+            var expectedUser = GetUserIdentity(id: id);
+
+            // Act
+            var actualUser = await _service.GetOrCreate(expectedUser.SlackUserId, expectedUser.SlackTeamId, expectedUser.SlackUserName);
+
+            // Assert
+            _idProviderMock.Verify(x => x.GenerateNewId(), Times.Once);
+            actualUser.AssertIsEqualTo(expectedUser);
+            var table = _storageAccount.GetTable(_service.UserIdentityByIdTableName);
+            table.Retrieve<DefaultNancyUserIdentityService.NancyUserIdentityTableEntity>(id.ToString(), id.ToString())
+                .GetUserIdentity()
+                .AssertIsEqualTo(expectedUser);
+            table = _storageAccount.GetTable(_service.UserIdentityBySlackUserIdTableName);
+            table.Retrieve<DefaultNancyUserIdentityService.NancyUserIdentityTableEntity>(expectedUser.SlackUserId, expectedUser.SlackUserId)
+                .GetUserIdentity()
+                .AssertIsEqualTo(expectedUser);
+        }
+
+        [Test]
+        public async Task GetOrCreateReturnsUserIdentityWhenUserExists()
+        {
+            // Arrange
+            var expectedUser = GetUserIdentity();
+            var table = _storageAccount.GetTable(_service.UserIdentityBySlackUserIdTableName);
+            table.Insert(new DefaultNancyUserIdentityService.NancyUserIdentityTableEntity(expectedUser, x => x.SlackUserId));
+
+            // Act
+            var actualUser = await _service.GetOrCreate(expectedUser.SlackUserId, expectedUser.SlackTeamId, expectedUser.SlackUserName);
+
+            // Assert
+            _idProviderMock.Verify(x => x.GenerateNewId(), Times.Never);
+            actualUser.AssertIsEqualTo(expectedUser);
+        }
+
+        [Test]
+        public void GetUserFromIdentifierReturnsNullWhenUserDoesNotExist()
+        {
+            // Arrange
+
+            // Act
+            var user = _service.GetUserFromIdentifier(Guid.NewGuid(), new NancyContext());
+
+            // Assert
+            Assert.That(user, Is.Null);
+        }
+
+        private static NancyUserIdentity GetUserIdentity(
+            Guid? id = null, 
+            string slackUserId = "slackUserId", 
+            string slackTeamId = "slackTeamId",
+            string slackUserName = "slackUserName")
+        {
+            return new NancyUserIdentity
+            {
+                Id = id ?? Guid.NewGuid(),
+                SlackUserId = slackUserId,
+                SlackTeamId = slackTeamId,
+                SlackUserName = slackUserName
+            };
+        }
+    }
+
+    public static class NancyUserIdentityExtensions
+    {
+        public static void AssertIsEqualTo(this NancyUserIdentity actualUserIdentity, NancyUserIdentity expectedUserIdentity)
+        {
+            Assert.That(actualUserIdentity, Is.Not.Null);
+            Assert.That(actualUserIdentity.Id, Is.EqualTo(expectedUserIdentity.Id));
+            Assert.That(actualUserIdentity.SlackUserId, Is.EqualTo(expectedUserIdentity.SlackUserId));
+            Assert.That(actualUserIdentity.SlackTeamId, Is.EqualTo(expectedUserIdentity.SlackTeamId));
+            Assert.That(actualUserIdentity.SlackUserName, Is.EqualTo(expectedUserIdentity.SlackUserName));
+        }
+    }
+}
--- src/SlashTodo.Web/Security/DefaultNancyUserIdentityService.cs
+++ src/SlashTodo.Web/Security/DefaultNancyUserIdentityService.cs
@@ -5,39 +5,46 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using Nancy.Authentication.Forms;
 using Nancy.Security;
+using SlashTodo.Infrastructure.AzureTables;
 using SlashTodo.Infrastructure.Messaging;
 
 namespace SlashTodo.Web.Security
 {
     public class DefaultNancyUserIdentityService : 
         INancyUserIdentityService, 
-        IUserMapper,
-        ISubscriber
+        IUserMapper
     {
         public const string DefaultUserIdentityByIdTableName = "nancyUserIdentityById";
         public const string DefaultUserIdentityBySlackUserIdTableName = "nancyUserIdentityBySlackUserId";
 
         private readonly CloudStorageAccount _storageAccount;
+        private readonly INancyUserIdentityIdProvider _idProvider;
         private readonly string _userIdentityByIdTableName;
         private readonly string _userIdentityBySlackUserIdTableName;
-        private readonly List<ISubscriptionToken> _subscriptionTokens = new List<ISubscriptionToken>();
 
         public string UserIdentityByIdTableName { get { return _userIdentityByIdTableName; } }
         public string UserIdentityBySlackUserIdTableName { get { return _userIdentityBySlackUserIdTableName; } }
 
-        public DefaultNancyUserIdentityService(CloudStorageAccount storageAccount)
-            : this(storageAccount, DefaultUserIdentityByIdTableName, DefaultUserIdentityBySlackUserIdTableName)
+        public DefaultNancyUserIdentityService(
+            CloudStorageAccount storageAccount, 
+            INancyUserIdentityIdProvider idProvider)
+            : this(storageAccount, idProvider, DefaultUserIdentityByIdTableName, DefaultUserIdentityBySlackUserIdTableName)
         {
         }
 
         public DefaultNancyUserIdentityService(
-            CloudStorageAccount storageAccount, 
+            CloudStorageAccount storageAccount,
+            INancyUserIdentityIdProvider idProvider,
             string userIdentityByIdTableName, 
             string userIdentityBySlackUserIdTableName)
         {
             if (storageAccount == null)
             {
                 throw new ArgumentNullException("storageAccount");
+            }
+            if (idProvider == null)
+            {
+                throw new ArgumentNullException("idProvider");
             }
             if (string.IsNullOrWhiteSpace(userIdentityByIdTableName))
             {
@@ -48,34 +55,40 @@ namespace SlashTodo.Web.Security
                 throw new ArgumentNullException("userIdentityBySlackUserIdTableName");
             }
             _storageAccount = storageAccount;
+            _idProvider = idProvider;
             _userIdentityByIdTableName = userIdentityByIdTableName;
             _userIdentityBySlackUserIdTableName = userIdentityBySlackUserIdTableName;
         }
 
-        public Task<NancyUserIdentity> GetOrCreate(string slackUserId, string slackTeamId, string slackUserName)
+        public async Task<NancyUserIdentity> GetOrCreate(string slackUserId, string slackTeamId, string slackUserName)
         {
-            // TODO
-            throw new NotImplementedException();
+            var table = await _storageAccount.GetTableAsync(_userIdentityBySlackUserIdTableName).ConfigureAwait(false);
+            var entity = await table.RetrieveAsync<NancyUserIdentityTableEntity>(slackUserId, slackUserId).ConfigureAwait(false);
+            if (entity != null)
+            {
+                return entity.GetUserIdentity();
+            }
+            var userIdentity = new NancyUserIdentity
+            {
+                Id = _idProvider.GenerateNewId(),
+                SlackUserId = slackUserId,
+                SlackTeamId = slackTeamId,
+                SlackUserName = slackUserName
+            };
+            var insertBySlackUserIdTask = table.InsertAsync(new NancyUserIdentityTableEntity(userIdentity, x => x.SlackUserId));
+            insertBySlackUserIdTask.ConfigureAwait(false);
+            table = await _storageAccount.GetTableAsync(_userIdentityByIdTableName).ConfigureAwait(false);
+            var insertByIdTask = table.InsertAsync(new NancyUserIdentityTableEntity(userIdentity, x => x.Id.ToString()));
+            insertByIdTask.ConfigureAwait(false);
+            await Task.WhenAll(insertBySlackUserIdTask, insertByIdTask).ConfigureAwait(false);
+            return userIdentity;
         }
 
         public IUserIdentity GetUserFromIdentifier(Guid identifier, Nancy.NancyContext context)
         {
-            // TODO
-            throw new NotImplementedException();
-        }
-
-        public void RegisterSubscriptions(ISubscriptionRegistry registry)
-        {
-            // TODO
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
-        {
-            foreach (var token in _subscriptionTokens)
-            {
-                token.Dispose();
-            }
+            var table = _storageAccount.GetTable(_userIdentityByIdTableName);
+            var entity = table.Retrieve<NancyUserIdentityTableEntity>(identifier.ToString(), identifier.ToString());
+            return entity != null ? entity.GetUserIdentity() : null;
         }
 
         public class NancyUserIdentityTableEntity : TableEntity
