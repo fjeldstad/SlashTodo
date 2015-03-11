4dfd2b6529ab3ca2ff779dc14e5428b19d220c7a
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-03-11 11:28:06 +0100

    Added dummy slash command handler.

---
+++ src/SlashTodo.Web/Api/DefaultSlashCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SlashTodo.Web.Api
+{
+    public class DefaultSlashCommandHandler : ISlashCommandHandler
+    {
+        public Task<string> Handle(SlashCommand command)
+        {
+            return Task.FromResult("*TODO*");
+        }
+    }
+}
