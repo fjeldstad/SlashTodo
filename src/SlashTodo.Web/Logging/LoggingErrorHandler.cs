981ebd24627ce3a9035e3b541f8d19c92e672a0f
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2015-03-11 13:23:21 +0100

    Add url tag to Sentry log entries.

--- src/SlashTodo.Web/Logging/LoggingErrorHandler.cs
+++ src/SlashTodo.Web/Logging/LoggingErrorHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.Extensions;
@@ -8,6 +10,12 @@ namespace SlashTodo.Web.Logging
 {
     public class LoggingErrorHandler : IStatusCodeHandler
     {
+        private static readonly HttpStatusCode[] InterestingStatusCodes = new[]
+        {
+            HttpStatusCode.InternalServerError,
+            HttpStatusCode.BadRequest,
+            HttpStatusCode.ServiceUnavailable
+        };
         private readonly ILogger _logger;
 
         public LoggingErrorHandler(ILogger logger)
@@ -17,20 +25,25 @@ namespace SlashTodo.Web.Logging
 
         public void Handle(HttpStatusCode statusCode, NancyContext context)
         {
+            var tags = new Dictionary<string, string>();
+            tags.Add("statusCode", string.Format("{0} {1}", (int)statusCode, statusCode));
+            if (context != null && context.Request != null && context.Request.Url != null)
+            {
+                tags.Add("url", context.Request.Url);
+            }
+
             Exception exception = null;
             if (context.TryGetException(out exception))
             {
-                _logger.LogException(exception, tags: new Dictionary<string, string>{{ "statusCode", statusCode.ToString() }});
+                _logger.LogException(exception, tags: tags);
             }
-            else
-            {
-                // TODO Well...
-            }
+
+            // TODO Log non-exceptions?
         }
 
         public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
         {
-            return statusCode == HttpStatusCode.InternalServerError;
+            return InterestingStatusCodes.Contains(statusCode);
         }
     }
 }
